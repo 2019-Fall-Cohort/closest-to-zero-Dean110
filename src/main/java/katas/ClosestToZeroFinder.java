0299a0ad7507c295f3ab559ba1d5e8969e33116e
Columbus Instructor <instructor@wecancodeit.org>
2019-11-18 17:04:45 -0500

    Closest to Zero

--- src/main/java/katas/ClosestToZeroFinder.java
+++ src/main/java/katas/ClosestToZeroFinder.java
@@ -1,11 +1,23 @@
 package katas;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ClosestToZeroFinder {
 
-  public int findClosestToZero(List<Integer> integers) {
-    return 1;
-  }
+	public int findClosestToZero(List<Integer> integers) {
+		Integer closestToZero = Integer.MAX_VALUE;
+		Collections.sort(integers);
+		if(integers.get(integers.size()-1)==Integer.MIN_VALUE) {
+			return Integer.MIN_VALUE;
+		}
+		for (Integer integer : integers) {
+			
+			if (Math.abs(closestToZero) >= Math.abs(integer)&&integer !=Integer.MIN_VALUE) {
+				closestToZero = integer;
+			}
+		}
+		return closestToZero;
+	}
 
 }
--- src/test/java/katas/ClosestToZeroFinderTest.java
+++ src/test/java/katas/ClosestToZeroFinderTest.java
@@ -6,17 +6,38 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ClosestToZeroFinderTest {
-  @Test
-  public void givenAListWithOneAndTwo_ReturnsOne() throws Exception {
-    //Arrange
-    ClosestToZeroFinder underTest = new ClosestToZeroFinder();
-    List<Integer> testData = Arrays.asList(1,2);
-    //Act
-    int result = underTest.findClosestToZero(testData);
-    //Assert
-    assertThat(result, equalTo(1));
-  }
+	private ClosestToZeroFinder underTest;
+
+	@Before
+	public void setup() {
+		underTest = new ClosestToZeroFinder();
+	}
+
+	@Test
+	public void givenAListOfIntsReturnsClosestToZero() throws Exception {
+		expectClosestToZero(1, Arrays.asList(1, 2));
+		expectClosestToZero(2, Arrays.asList(2, 3));
+		expectClosestToZero(2, Arrays.asList(3, 2));
+		expectClosestToZero(1, Arrays.asList(2, 3, 1));
+		expectClosestToZero(1, Arrays.asList(-2, 1));
+		expectClosestToZero(2, Arrays.asList(-2, 2, -2, -2, -2, 2, 4, 5, 2009));
+		expectClosestToZero(0, Arrays.asList(0, 2, -2, 1));
+		expectClosestToZero(Integer.MAX_VALUE, Arrays.asList(Integer.MIN_VALUE, Integer.MAX_VALUE));
+		expectClosestToZero(Integer.MIN_VALUE, Arrays.asList(Integer.MIN_VALUE, Integer.MIN_VALUE));
+		expectClosestToZero(Integer.MIN_VALUE, Arrays.asList( Integer.MIN_VALUE));
+		
+
+
+	}
+
+	private void expectClosestToZero(int expectedValue, List<Integer> testData) {
+		int result = underTest.findClosestToZero(testData);
+		assertThat(result, equalTo(expectedValue));
+	}
+
 }
